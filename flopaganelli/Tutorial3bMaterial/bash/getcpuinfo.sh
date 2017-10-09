bfbb67dd6b2deaf843ba1608c6e025a5fe775a98
Jessamy <jessamy.mol@gmail.com>
2017-10-09 12:35:10 +0200

    saving all changes HW3b

--- flopaganelli/Tutorial3bMaterial/bash/getcpuinfo.sh
+++ flopaganelli/Tutorial3bMaterial/bash/getcpuinfo.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # put the output of cat in the variable CPUINFO
-CPUINFO=$(cat /proc/cpuinfo | head -10)
+CPUINFO=$(cat /proc/cpuinfo | head ­-10)
 
 # write the content of CPUINFO to screen
 echo "$CPUINFO"
